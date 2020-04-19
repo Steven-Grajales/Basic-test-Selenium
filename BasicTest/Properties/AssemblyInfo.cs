fc04d584145c7a08fd5cab77378bf59c5b46e1ed
Steven <steven.deibi@hotmail.com>
2020-04-19 10:16:12 -0500

    Creación del proyecto + test básico en Selenium

---
+++ BasicTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("BasicTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0274905c-e253-4762-8210-d05e05e463c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BasicTest/SeleniumTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BasicTest
+{
+    [TestClass]
+    public class SeleniumTest
+    {
+
+        [TestMethod]
+        public void setup() {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://localhost:44328/login.aspx");
+            Thread.Sleep(2000);
+
+            //Ingresar al login
+            driver.FindElement(By.Id("txtUserName")).SendKeys("admin");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("123");
+            driver.FindElement(By.Id("btnlogin")).Click();
+            Thread.Sleep(1000);
+
+            //Registro de usuario
+            driver.FindElement(By.Id("txtUserName")).SendKeys("Juan");
+            driver.FindElement(By.Id("txtPassword")).SendKeys("951");
+            driver.FindElement(By.Id("btnRegistrar")).Click();
+        }
+    }
+}
